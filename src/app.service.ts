7413d28053dd0ff7ac414cee9ce41984a06fb452
Ocee <ocjay24@gmail.com>
2024-07-02 09:14:18 +0100

    updated service

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,31 +12,40 @@ export class AppService {
   private readonly apiUrl: string = 'https://api.openweathermap.org/data/2.5/weather';
 
   constructor(
-    private readonly httpService: HttpService, 
+    private readonly httpService: HttpService,
     private readonly configService: ConfigService,
   ) {
-    this.apiKey = this.configService.get<string>('WEATHER_API')
+    this.apiKey = this.configService.get<string>('WEATHER_API');
   }
 
-
   async getClient(name: string, ip: string): Promise<any> {
-    const geo = geoip.lookup(ip);
-    const weather_data: AxiosResponse<any> = await lastValueFrom(
-      this.httpService.get(this.apiUrl, {
-        params: {
-          q: geo.city,
-          appid: this.apiKey,
-        },
-    }),
-  );
+    try {
+      const geo = geoip.lookup(ip);
+      if (!geo) {
+        throw new Error('Invalid IP address');
+      }
 
-    const temp = weather_data.data.main.temp
+      const weather_data: AxiosResponse<any> = await lastValueFrom(
+        this.httpService.get(this.apiUrl, {
+          params: {
+            q: geo.city,
+            appid: this.apiKey,
+          },
+        }),
+      );
 
-    const data = {
-      client_ip: ip,
-      location: geo.city,
-      greeting: `Hello, ${name}!, the temperature is ${temp} celcius in ${geo.city}`
+      const temp = weather_data.data.main.temp;
+
+      const data = {
+        client_ip: ip,
+        location: geo.city,
+        greeting: `Hello, ${name}! The temperature is ${temp} Celsius in ${geo.city}`,
+      };
+
+      return data;
+    } catch (error) {
+
+      throw new Error(`Error fetching weather data for IP ${ip}: ${error.message}`);
     }
-    return data;
   }
 }
